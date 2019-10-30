7c9364fd0f0e6c81b459d8153a53fb94767ed866
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2019-10-30 23:52:15 +0100

    Add routes + views

--- app.py
+++ app.py
@@ -1,21 +1,59 @@
 import os
 
-from flask import Flask, make_response, send_from_directory
+from flask import (
+    Flask,
+    make_response,
+    send_from_directory,
+    render_template,
+    redirect,
+    url_for,
+    jsonify,
+    request,
+)
 from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="views", static_folder="www/dist")
 
-
+# App Routes
 @app.route("/")
 def root():
-    return app.send_static_file("index.html")
+    return redirect(url_for("new"))
 
 
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+
+@app.route("/u")
+def universe():
+    id = request.args.get("id")
+    if id == None:
+        return redirect(url_for("new"))
+
+    return render_template("universe.html", id=id)
+
+
+# API
+@app.route("/api/universe/<id>", methods=["GET"])
+def get_universe(id):
+    # TODO
+    return jsonify(id=id)
+
+
+@app.route("/api/universe", methods=["POST"])
+def create_universe():
+    # TODO
+    return jsonify(id="TODO")
+
+
+# Static files
 @app.route("/<path:path>")
 def static_file(path):
     mimetypes = {
+        ".html": "text/html",
         ".css": "text/css; charset=utf-8",
         ".js": "application/javascript; charset=utf-8",
         ".wasm": "application/wasm",
